032da205ab5a0b10f8ff4a2daafaf6678c9e06a9
blascoibanez136 <gregjhanney@gmail.com>
2025-11-10 20:07:00 -0500

    Update nba_ingest.py

--- src/ingest/nba_ingest.py
+++ src/ingest/nba_ingest.py
@@ -7,14 +7,14 @@ BASE_URL = "https://v1.basketball.api-sports.io"
 
 def _headers():
     return {
-        "x-apisports-key": 85d6d09b8a3a8322136eae8c59aa0148,
+        "x-apisports-key": API_KEY,
     }
 
 
 def get_games_by_date(date_str: str):
     """
     Fetch games for a given date from API-Sports Basketball
-    date_str example: '2025-11-10' or '2024-12-25'
+    date_str example: '2025-11-10'
     """
     url = f"{BASE_URL}/games"
     params = {"date": date_str}
